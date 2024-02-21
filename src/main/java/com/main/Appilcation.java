ab114037fa3d88631d2bec02b59c8fef8e364e68
Teju-L <144414084+Teju-L@users.noreply.github.com>
2024-02-21 11:01:27 +0530

    Update Appilcation.java

--- src/main/java/com/main/Appilcation.java
+++ src/main/java/com/main/Appilcation.java
@@ -3,7 +3,7 @@ package com.main;
 public class Appilcation {
 
 	public static void main(String[] args) {
-			String s = "jenkins demo";
+			String s = "jenkins demo1";
 			System.out.println("Welcome to "+s);;
 		}
 }
